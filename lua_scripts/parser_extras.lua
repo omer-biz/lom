d78a18afca9b38fb81919c1c306be8bd6455882b
omer <omerabdi@protonmail.com>
2025-10-29 21:15:44 +0300

    feat: deep compare two tables

--- lua_scripts/parser_extras.lua
+++ lua_scripts/parser_extras.lua
@@ -49,4 +49,33 @@ function M.utils.print(t, indent)
   print(spacing .. "},")
 end
 
+function M.utils.tables_equal(t1, t2, visited)
+    visited = visited or {}
+    if visited[t1] and visited[t1][t2] then
+        return true
+    end
+
+    if t1 == t2 then return true end
+    if type(t1) ~= "table" or type(t2) ~= "table" then return false end
+
+    visited[t1] = visited[t1] or {}
+    visited[t1][t2] = true
+
+    for k, v in pairs(t1) do
+        local v2 = t2[k]
+        if type(v) == "table" and type(v2) == "table" then
+            if not M.utils.tables_equal(v, v2, visited) then return false end
+        elseif v2 ~= v then
+            return false
+        end
+    end
+
+    -- check for extra keys in t2
+    for k, _ in pairs(t2) do
+        if t1[k] == nil then return false end
+    end
+    return true
+end
+
+
 return M
