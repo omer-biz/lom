6a1212f1165637e19151331f9aa916c940bf39e9
omer <omerabdi@protonmail.com>
2025-11-03 22:03:57 +0300

    feat: inspect higher-order combinators

--- src/inspect.c
+++ src/inspect.c
@@ -1,6 +1,7 @@
 #include "inspect.h"
 #include "parser.h"
 #include "utils.h"
+#include <stdio.h>
 
 static char *inspect_binary(const char *name, Parser *left, Parser *right,
                             int indent) {
@@ -53,8 +54,12 @@ static char *inspect_one_or_more(Parser *p, int indent) {
       snprintf(NULL, 0, "%sone_or_more(\n%s\n%s)\n", ind, inner, ind) + 1;
 
   char *buff = malloc(size);
-  if (!buff)
+  if (!buff) {
+  free(ind);
+  free(inner);
     return NULL;
+
+  }
   snprintf(buff, size, "%sone_or_more(\n%s\n%s)\n", ind, inner, ind);
 
   free(ind);
@@ -62,7 +67,6 @@ static char *inspect_one_or_more(Parser *p, int indent) {
 
   return buff;
 }
-
 
 static char *inspect_zero_or_more(Parser *p, int indent) {
   RepData *d = (RepData *)p->data;
@@ -74,14 +78,62 @@ static char *inspect_zero_or_more(Parser *p, int indent) {
       snprintf(NULL, 0, "%szero_or_more(\n%s\n%s)\n", ind, inner, ind) + 1;
 
   char *buff = malloc(size);
-  if (!buff)
+  if (!buff) {
+    free(ind);
+    free(inner);
     return NULL;
+  }
+
   snprintf(buff, size, "%szero_or_more(\n%s\n%s)\n", ind, inner, ind);
 
   free(ind);
   free(inner);
 
   return buff;
+}
+
+static char *inspect_unary_with_func(const char *name, Parser *inner,
+                                     int func_ref, int indent) {
+  lua_State *L = inner->L; // it's all the same, I think. Might be different if
+                           // we are going to use threads
+  char *ind = make_indent(indent);
+  char *inner_str = inspect_parser(inner, indent + 1);
+  char *func_desc = describe_lua_function(L, func_ref);
+
+  int size = snprintf(NULL, 0, "%s%s(%s\n%s%s)\n", ind, name, func_desc,
+                      inner_str, ind) +
+             1;
+  char *buff = malloc(size);
+  if (!buff) {
+    free(ind);
+    free(inner_str);
+    free(func_desc);
+    return NULL;
+  }
+
+  snprintf(buff, size, "%s%s(%s\n%s%s)\n", ind, name, func_desc, inner_str,
+           ind);
+
+  free(ind);
+  free(inner_str);
+  free(func_desc);
+
+  return buff;
+}
+
+static char *inspect_pred(Parser *p, int indent) {
+  PredData *d = (PredData *)p->data;
+  return inspect_unary_with_func("red", d->inner, d->func_ref, indent);
+}
+
+static char *inspect_map(Parser *p, int indent) {
+  MapData *d = (MapData *)p->data;
+  return inspect_unary_with_func("map", d->inner, d->func_ref, indent);
+}
+
+static char *inspect_and_then(Parser *p, int indent) {
+  AndThenData *d = (AndThenData *)p->data;
+  return inspect_unary_with_func("and_then", d->inner, d->func_ref, indent);
 }
 
 static char *inspect_parser(Parser *p, int indent) { return ""; }
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <lauxlib.h>
+#include <lua.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifndef __LOM_UTILS
 #define __LOM_UTILS
 
@@ -17,6 +22,29 @@ static char *make_indent(int level) {
   memset(idnt, ' ', n);
   idnt[n] = '\0';
   return idnt;
+}
+
+
+static char *describe_lua_function(lua_State *L, int func_ref) {
+  lua_rawgeti(L, LUA_REGISTRYINDEX, func_ref);
+
+  lua_Debug ar;
+  const char *name = "<anonymous>";
+  int line = 0;
+
+  if (lua_getinfo(L, ">nS", &ar)) {
+    if (ar.name)
+      name = ar.name;
+    line = ar.linedefined;
+  }
+
+  int size = snprintf(NULL, 0, "%s@%s:%d", name, ar.short_src, line) + 1;
+  char *buff = malloc(size);
+  if (buff)
+    snprintf(buff, size, "%s@%s:%d", name, ar.short_src, line);
+
+  lua_pop(L, 1);
+  return buff;
 }
 
 #endif
